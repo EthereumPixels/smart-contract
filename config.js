5f0c3561e0cb4b745cf95380e8147317b7595f48
Raven Jiang <raven@cs.stanford.edu>
2017-07-11 18:20:15 +0800

    Change initialization configs

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const web3 = new Web3();
 
 module.exports = {
   GRID_SIZE: 1000,
-  DEFAULT_PRICE: web3.toBigNumber(web3.toWei(0.01, 'ether')),
-  FEE_RATIO: 100,
-  INCREMENT_RATE: 50,
+  DEFAULT_PRICE: web3.toBigNumber(web3.toWei(0.002, 'ether')),
+  FEE_RATIO: 40,
+  INCREMENT_RATE: 25,
 };
